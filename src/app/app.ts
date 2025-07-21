f7381b23e01d83c2675fe97b85000cd8625b38f5
kanafitk <kanafitk@gmail.com>
2025-07-21 21:24:09 +0700

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './navbar/navbar';
+import {Slide} from './slide/slide';
+import {Product} from './product/product';
+import {Footer} from './footer/footer';
+
+// interface product {
+//   name_: string;
+//   detail: string;
+//   price: number;
+//   discount: number;
+//   stock: number;
+// }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,Navbar,Slide,Product,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
